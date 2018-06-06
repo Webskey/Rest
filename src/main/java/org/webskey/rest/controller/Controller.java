55ddabfd0124009c7a657e10c1abda872dc0ed2e
Webskey <jolo_92@wp.pl>
2018-06-06 19:32:35 +0200

    added ActivityService class

--- src/main/java/org/webskey/rest/controller/Controller.java
+++ src/main/java/org/webskey/rest/controller/Controller.java
@@ -1,6 +1,5 @@
 package org.webskey.rest.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -16,7 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.webskey.rest.model.ActivityEntity;
-import org.webskey.rest.repository.ActivityRepository;
+import org.webskey.rest.service.ActivityService;
 
 @RestController
 public class Controller {
@@ -24,25 +23,23 @@ public class Controller {
 	private static Logger log = LogManager.getLogger(Controller.class);
 
 	@Autowired
-	private ActivityRepository activityRepository;
+	private ActivityService activityService;
 
 	// GET
 	@GetMapping("/activities/{id}")
 	public ResponseEntity<?> getActivityById(@PathVariable int id) {			
-		if(!activityRepository.findById(id).isPresent()) {
+		if(!activityService.exists(id)) {
 			log.error("Rerieving activity details with id: {} - negative(no such id).", id);
 			return new ResponseEntity<String>("Activity with id: " + id + " not found.",
 					HttpStatus.NOT_FOUND);	
 		}
 		log.info("Rerieving activity details with id: {} - positive.", id);
-		return new ResponseEntity<ActivityEntity>(activityRepository.findById(id).get(), HttpStatus.OK);
+		return new ResponseEntity<ActivityEntity>(activityService.findById(id), HttpStatus.OK);
 	}
 
 	@GetMapping("/activities/list")
 	public ResponseEntity<?> getAllActivities() {
-		List<ActivityEntity> list = new ArrayList<>();
-		for(ActivityEntity row : activityRepository.findAll())
-			list.add(row);
+		List<ActivityEntity> list = activityService.findAll();		
 		if(list.isEmpty()) {
 			log.error("Retrieving all activities failed. List is empty");
 			return new ResponseEntity<String>("No activity found.", HttpStatus.NO_CONTENT);
@@ -53,68 +50,57 @@ public class Controller {
 	// SAVE
 	@PostMapping("/activities/save")
 	public ResponseEntity<?> saveActivity(@RequestBody ActivityEntity activity) {
-		if(activityRepository.existsById(activity.getId())) {
+		if(activityService.exists(activity.getId())) {
 			log.error("Saving new activity with id: {} - negative(already exists).", activity.getId());
 			return new ResponseEntity<String>("Activity with id: " + activity.getId() + " already exists.",
 					HttpStatus.CONFLICT);
 		}
-		activityRepository.save(activity);
+		activityService.save(activity);
 		log.info("Saving new activity with id: {} - positive.", activity.getId());
 		return new ResponseEntity<ActivityEntity>(activity, HttpStatus.CREATED);
 	}
 
 	@PostMapping("/activities/saveAll")
 	public ResponseEntity<?> saveAllActivities(@RequestBody List<ActivityEntity> list){
-		StringBuilder sb = new StringBuilder();
-		boolean flag = false;
-		for(ActivityEntity activity : list) {
-			if(activityRepository.existsById(activity.getId())) {
-				sb.append(" " + activity.getId() + ",");
-				flag = true;
-			}
+		String duplicates = activityService.findDuplicates(list);
+		if(!duplicates.isEmpty()) {			
+			log.error("Saving new activities failed. Activities with id:{}already exists.", duplicates);
+			return new ResponseEntity<String>("Activities with id:" + duplicates + "already exists.", HttpStatus.CONFLICT);
 		}
-		if(flag) {
-			log.error("Saving new activities failed. Activities with id:{}already exists.", sb);
-			return new ResponseEntity<String>("Activities with id:" + sb + "already exists.", HttpStatus.CONFLICT);
-		}
-		activityRepository.saveAll(list);
+		activityService.saveAll(list);
 		log.info("Saving new {} activities - positive.", list.size());
 		return new ResponseEntity<List<ActivityEntity>>(list, HttpStatus.CREATED);
 	}	
 	// DELETE
 	@DeleteMapping("/activities/delete/{id}")
 	public ResponseEntity<?> deleteActivityById(@PathVariable int id){
-		if(!activityRepository.existsById(id)) {
+		if(!activityService.exists(id)) {
 			log.info("Deleting activity with id: {} - negative(no such id).", id);
 			return new ResponseEntity<String>("Activity with id: " + id + " not found.", HttpStatus.NOT_FOUND);
 		}
-		activityRepository.deleteById(id);
+		activityService.deleteById(id);
 		log.info("Deleting activity with id: {} - positive.", id);
 		return new ResponseEntity<ActivityEntity>(HttpStatus.NO_CONTENT);
 	}
 
 	@DeleteMapping("/activities/deleteAll")
 	public ResponseEntity<?> deleteAllActivities(){
-		if(activityRepository.count() == 0) {
+		if(activityService.count() == 0) {
 			log.error("Deleting all activities - negative(list of activities is empty).");
 			return new ResponseEntity<String>("List of activities is empty.", HttpStatus.NOT_FOUND);
 		}
-		activityRepository.deleteAll();
+		activityService.deleteAll();
 		log.info("Deleting all activities - positive.");
 		return new ResponseEntity<ActivityEntity>(HttpStatus.NO_CONTENT);
 	}
 	// UPDATE
 	@PutMapping("/activities/update/{id}")
 	public ResponseEntity<?> updateActivityById(@PathVariable int id, @RequestBody ActivityEntity activity){
-		if(!activityRepository.findById(id).isPresent()) {
+		if(!activityService.exists(id)) {
 			log.error("Updating activity with id: {} - negative(no such activity).", id);
 			return new ResponseEntity<String>("Activity with id: " + id + " not found.", HttpStatus.NOT_FOUND);
-		}
-		ActivityEntity currActivity = activityRepository.findById(id).get();
-		currActivity.setName(activity.getName());
-		currActivity.setDesc(activity.getDesc());
-		currActivity.setNum(activity.getNum());
-		activityRepository.save(currActivity);
+		}		
+		activityService.update(id, activity);
 		log.info("Updating activity with id: {} - positive.", id);
 		return new ResponseEntity<ActivityEntity>(HttpStatus.OK);
 	}
---
+++ src/main/java/org/webskey/rest/service/ActivityService.java
@@ -0,0 +1,69 @@
+package org.webskey.rest.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.webskey.rest.model.ActivityEntity;
+import org.webskey.rest.repository.ActivityRepository;
+
+@Service
+public class ActivityService {
+
+	@Autowired
+	private ActivityRepository activityRepository;
+
+	public boolean exists(int id) {
+		return activityRepository.existsById(id);
+	}
+
+	public String findDuplicates(List<ActivityEntity> list) {
+		StringBuilder sb = new StringBuilder();		
+		for(ActivityEntity activity : list) {
+			if(exists(activity.getId())) {
+				sb.append(" " + activity.getId() + ",");				
+			}
+		}
+		return sb.toString();
+	}
+
+	public ActivityEntity findById(int id) {
+		return activityRepository.findById(id).get();
+	}
+
+	public List<ActivityEntity> findAll(){
+		List<ActivityEntity> list = new ArrayList<>();
+		for(ActivityEntity row : activityRepository.findAll())
+			list.add(row);
+		return list;
+	}
+
+	public void save(ActivityEntity activity) {
+		activityRepository.save(activity);
+	}
+
+	public void saveAll(List<ActivityEntity> list) {
+		activityRepository.saveAll(list);
+	}
+
+	public long count() {
+		return activityRepository.count();
+	}
+
+	public void deleteAll() {
+		activityRepository.deleteAll();
+	}
+
+	public void deleteById(int id) {
+		activityRepository.deleteById(id);
+	}
+
+	public void update(int id, ActivityEntity activity) {
+		ActivityEntity currActivity = activityRepository.findById(id).get();	
+		currActivity.setName(activity.getName());
+		currActivity.setDesc(activity.getDesc());
+		currActivity.setNum(activity.getNum());
+		activityRepository.save(currActivity);
+	}
+}
